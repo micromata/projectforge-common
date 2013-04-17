bde0a76a40474c0abb21f54bee4f8c7675946b6a
Kai Reinhard <K.Reinhard@micromata.de>
2013-04-17 06:48:33 +0200

    Initial revision.

--- src/main/java/org/projectforge/ProjectForgeVersion.java
+++ src/main/java/org/projectforge/ProjectForgeVersion.java
@@ -17,10 +17,10 @@ public class ProjectForgeVersion
 
   public static final String YEAR = "2013";
 
-  public static final String VERSION_STRING = "5.0";
+  public static final String VERSION_STRING = "5.0.1-SNAPSHOT";
 
-  public static final String RELEASE_DATE = "2013-04-12";
+  public static final String RELEASE_DATE = "2013-04-17";
 
-  public static final String RELEASE_TIMESTAMP = "2013-04-12 05:56";
+  public static final String RELEASE_TIMESTAMP = "2013-04-17 06:42";
 
 }
