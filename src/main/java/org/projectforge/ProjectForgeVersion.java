c4a35acc12795621cf9cfeed9ff9f4fbe5efc1d1
Kai Reinhard <K.Reinhard@micromata.de>
2014-05-13 22:26:19 +0200

    Timestamp

--- src/main/java/org/projectforge/ProjectForgeVersion.java
+++ src/main/java/org/projectforge/ProjectForgeVersion.java
@@ -19,8 +19,8 @@ public class ProjectForgeVersion
 
   public static final String VERSION_STRING = "5.4";
 
-  public static final String RELEASE_DATE = "2014-05-11";
+  public static final String RELEASE_DATE = "2014-05-13";
 
-  public static final String RELEASE_TIMESTAMP = "2014-05-11 11:53";
+  public static final String RELEASE_TIMESTAMP = "2014-05-13 21:37";
 
 }
